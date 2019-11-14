b09f65e1dc8a1648e1b9bd54e45e4e6bf9d06a75
Joshua Berger <jmberger789@gmail.com>
2019-11-13 21:19:11 -0600

    developed move matrix, removed games with only 1 move

--- chess-project.js
+++ chess-project.js
@@ -4,25 +4,34 @@ function plot_it()  {
 		d.rated = Boolean(d.rated)
 		d.turns = +d.turns
 		d.white_rating = +d.white_rating
-		d.black_rating = + d.black_rating
+		d.black_rating = +d.black_rating
 		d.moves = d.moves.split(" ")
+		//if (typeof d.moves[1] === 'undefined') {console.log(d)}
+		//console.log(d.winner)
+		//d.winner = String(d.winner)
 	});
 
 	opening_move_matrix = {}
-	for game in chess_data {
-		game.moves[0]
+	for (game of chess_data) {
+		//console.log(game.moves[0])
+		if (typeof opening_move_matrix[game.moves[0]] === 'undefined') 
+			{opening_move_matrix[game.moves[0]] = {}}
 	}
-	{
-		similarity_matrix.push([])
-		for (j = 0; j < sentence.length; j++)
-		{
-			dot_product = 0
-			for (dimension of lstm_states)
-			{
-				dot_product += dimension[i]*dimension[j]
-			}
-			similarity_matrix[i].push(dot_product)
+	for (move in opening_move_matrix) {
+		for (game of chess_data) {
+			opening_move_matrix[move][game.moves[1]] = {'val': null, 'total_games': 0}
 		}
 	}
-
+	for (game of chess_data) {
+		if (opening_move_matrix[game.moves[0]][game.moves[1]]['total_games'] == 0) {opening_move_matrix[game.moves[0]][game.moves[1]]['val'] = 0}
+		if (game.winner == 'white') {opening_move_matrix[game.moves[0]][game.moves[1]]['val'] += 1}
+		else if (game.winner == 'black') {opening_move_matrix[game.moves[0]][game.moves[1]]['val'] -= 1}
+		opening_move_matrix[game.moves[0]][game.moves[1]]['total_games'] += 1
+	}
+	for (move1 in opening_move_matrix) {
+		for (move2 in opening_move_matrix[move1]) {
+			opening_move_matrix[move1][move2]['val'] /= opening_move_matrix[move1][move2]['total_games']
+		
+		}
+	}
 }
